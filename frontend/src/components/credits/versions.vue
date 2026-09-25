d78818ce8040e198481859299e24558b8a6a4ac5
Ts-Final <139440186+Ts-Final@users.noreply.github.com>
2026-09-25 20:52:16 +0800

    [Frontend][Server] Version update

--- frontend/src/components/credits/versions.vue
+++ frontend/src/components/credits/versions.vue
@@ -4,13 +4,13 @@ import Build from '@components/credits/build.vue'
 
 <template>
   <div class="version-wrapper">
-    <Build build="1.0" d="24" m="9" y="2026" title="0.1.0">
+    <Build build="1.0" d="24" m="9" title="0.1.0" y="2026">
       <div>Hello world.</div>
     </Build>
-    <Build build="2" d="24" m="9" y="2026" title="0.1.1">
-      <div>
-
-      </div>
+    <Build build="2" d="24" m="9" title="0.1.1" y="2026">
+      <div>添加了导出png。</div>
+      <div>添加了竖直方向的吸附，可以在设置中设置最大分割。</div>
+      <div>添加了前端文件的自动更新。如果你访问不了github可能就有点难受了。</div>
     </Build>
   </div>
 </template>
